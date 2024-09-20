e60a146288ca2c2bb804ea76f24da2819ce56482
degiwo <d.wong93@hotmail.de>
2024-09-20 22:21:42 +0200

    create variables for column names

--- train_model.py
+++ train_model.py
@@ -1,11 +1,16 @@
 from sklearn.datasets import load_iris
 import seaborn as sns
 
+COL_SEPAL_LENGTH = "sepal length (cm)"
+COL_SEPAL_WIDTH = "sepal width (cm)"
+COL_PETAL_LENGTH = "petal length (cm)"
+COL_PETAL_WIDTH = "petal width (cm)"
+
 X, y = load_iris(return_X_y=True, as_frame=True)
 
 sns.scatterplot(
     data=X,
-    x="sepal length (cm)",
-    y="sepal width (cm)",
+    x=COL_PETAL_LENGTH,
+    y=COL_SEPAL_WIDTH,
     hue=y,
 )
