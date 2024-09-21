a3e947b9ce20f86ccc931117e10e833a3f344397
degiwo <d.wong93@hotmail.de>
2024-09-21 09:51:35 +0200

    create plot and streamlit app for eda

--- train_model.py
+++ train_model.py
@@ -1,5 +1,8 @@
 from sklearn.datasets import load_iris
+from matplotlib.axes._axes import Axes
 import seaborn as sns
+import matplotlib.pyplot as plt
+import streamlit as st
 
 COL_SEPAL_LENGTH = "sepal length (cm)"
 COL_SEPAL_WIDTH = "sepal width (cm)"
@@ -8,9 +11,20 @@ COL_PETAL_WIDTH = "petal width (cm)"
 
 X, y = load_iris(return_X_y=True, as_frame=True)
 
-sns.scatterplot(
-    data=X,
-    x=COL_PETAL_LENGTH,
-    y=COL_SEPAL_WIDTH,
-    hue=y,
+def plot(x) -> Axes:
+    return sns.scatterplot(
+        data=X,
+        x=x,
+        y=COL_SEPAL_WIDTH,
+        hue=y,
+    )
+
+ax = plot(x=COL_SEPAL_LENGTH)
+plt.savefig("iris_scatterplot.png")
+
+x = st.selectbox(
+    "x column", (COL_SEPAL_LENGTH, COL_SEPAL_WIDTH)
 )
+
+ax = plot(x)
+st.pyplot(ax.figure, clear_figure=True)
