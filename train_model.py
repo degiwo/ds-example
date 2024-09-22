8ae71022314acb639ccbdef9c6b75993944ea545
degiwo <46052821+degiwo@users.noreply.github.com>
2024-09-22 20:01:50 +0200

    Project base setup (#1)

--- train_model.py
+++ train_model.py
@@ -1,11 +1,24 @@
+import json
+import joblib
 from sklearn.datasets import load_iris
-import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+
+COL_SEPAL_LENGTH = "sepal length (cm)"
+COL_SEPAL_WIDTH = "sepal width (cm)"
+COL_PETAL_LENGTH = "petal length (cm)"
+COL_PETAL_WIDTH = "petal width (cm)"
 
 X, y = load_iris(return_X_y=True, as_frame=True)
 
-sns.scatterplot(
-    data=X,
-    x="sepal length (cm)",
-    y="sepal width (cm)",
-    hue=y,
-)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=123)
+
+model = LogisticRegression()
+model.fit(X_train, y_train)
+joblib.dump(model, "model.joblib")
+
+predictions = model.predict(X_test)
+accuracy = model.score(X_test, y_test)
+print(f"Accuracy: {accuracy:.2f}")
+with open("accuracy.json", "w") as f:
+    json.dump(accuracy, f)
